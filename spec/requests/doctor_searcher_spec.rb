f728c87227bf83930a852353a1b51e41a701759b
Anna Ho <llanna.holl@gmail.com>
2016-10-04 11:41:17 -0700

    test doctor_searcher api request

---
+++ spec/requests/doctor_searcher_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe DoctorSearcher, :type => :request, vcr: true do
+  it 'return doctors with name' do
+    result = DoctorSearcher.search("Emily Huang")
+    summary = JSON.parse(result)["meta"]
+    expect(summary).to eq({"data_type"=>"array", "item_type"=>"Doctor", "total"=>2, "count"=>2, "skip"=>0, "limit"=>10})
+  end
+end
