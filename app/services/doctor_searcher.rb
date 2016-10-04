2f85ceeccfedd6275f8b262e4b041c474abba08e
Anna Ho <llanna.holl@gmail.com>
2016-10-04 11:40:04 -0700

    modify doctor_searcher object initialization

--- app/services/doctor_searcher.rb
+++ app/services/doctor_searcher.rb
@@ -2,13 +2,6 @@ class DoctorSearcher
 
   BASE_URL = "https://api.betterdoctor.com/2016-03-01/doctors"
 
-  attr_reader :response, :status
-  
-  def initialize(response, status)
-    @response = response
-    @status = status
-  end
-
   def self.search(doctor_name)
     value = Rails.cache.fetch([BASE_URL,doctor_name], :expires_in => 30.minutes) do
        result = HTTParty.get(BASE_URL, :query=>{:name => doctor_name, :user_key=>Rails.application.secrets.better_doctor_api_key})
